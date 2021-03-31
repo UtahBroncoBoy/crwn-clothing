b46380f4f518c06378c91fbb86cde5cc221b2a41
Jeff Fredericks <jeff@Jeffs-MacBook-Air.local>
2021-03-31 08:54:16 -0600

    Made some changes to add redux to different areas of the site

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,9 +1,24 @@
 import { combineReducers } from 'redux';
+import { persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage'; //This is telling redux persist we want to use local storage as our default storage
 
-import userReducer from './user/user.reducer';
 import cartReducer from './cart/cart.reducer';
+import directoryReducer from './directory/directory.reducer';
+import shopReducer from './shop/shop.reducer';
+import userReducer from './user/user.reducer';
 
-export default combineReducers({
+//The object we use to set configurations for redux persist.
+const persistConfig = {
+    key: 'root',
+    storage,
+    whitelist: ['cart'] 
+};
+
+const rootReducer = combineReducers({
     cart: cartReducer,
-    user: userReducer
+    directory: directoryReducer,
+    shop: shopReducer,
+    user: userReducer,
 });
+
+export default persistReducer(persistConfig, rootReducer);
